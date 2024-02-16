e5b4c2e1ca2543ef75948e2971e668b15657e341
fhluo <20843637+fhluo@users.noreply.github.com>
2024-02-16 22:57:47 +0800

    feat: add icon

---
+++ assets/gen_ico.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+from PIL import Image
+
+if __name__ == '__main__':
+    path = Path(__file__).resolve()
+
+    img = Image.open(path.with_name("xpic.png"))
+    img.save(path.with_name("xpic.ico"))
