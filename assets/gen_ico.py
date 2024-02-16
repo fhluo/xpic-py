bceced92a4dda3f8920b68398a3e10965a2aeb4e
fhluo <20843637+fhluo@users.noreply.github.com>
2024-02-17 00:52:16 +0800

    style: use double quotes

--- assets/gen_ico.py
+++ assets/gen_ico.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 from PIL import Image
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     path = Path(__file__).resolve()
 
     img = Image.open(path.with_name("xpic.png"))
