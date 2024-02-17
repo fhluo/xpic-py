17600b97a803d5dc0d73cdbf8dca74547ad369e7
fhluo <20843637+fhluo@users.noreply.github.com>
2024-02-18 01:30:41 +0800

    feat: add command line interface

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,41 @@
 use std::path::PathBuf;
+use clap::{Parser, Subcommand};
 
 mod image_util;
 mod spotlight;
 
-fn main() {
-    spotlight::get_images()
-        .unwrap()
-        .into_iter()
-        .for_each(|path| println!("{}", path.display()));
 
-    spotlight::copy_images_to(&PathBuf::from("./temp/images"), true).unwrap();
+#[derive(Parser)]
+#[command(version, about, arg_required_else_help(true))]
+struct CLI {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// List wallpapers
+    List {},
+    /// Save wallpapers
+    Save {
+        /// The directory where wallpapers are saved
+        dir: PathBuf
+    },
+}
+
+fn main() {
+    let cli = CLI::parse();
+
+
+    if let Some(command) = &cli.command {
+        match command {
+            Commands::List {} => spotlight::get_images()
+                .unwrap()
+                .into_iter()
+                .for_each(|path| println!("{}", path.display())),
+            Commands::Save { dir } => {
+                spotlight::copy_images_to(dir, true).unwrap();
+            }
+        }
+    }
 }
