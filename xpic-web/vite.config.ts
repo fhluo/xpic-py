ed08195af5365a4b8903a7af09334374fc6c7725
fhluo <20843637+fhluo@users.noreply.github.com>
2024-02-21 02:54:42 +0800

    feat: add xpic-tauri

--- xpic-web/vite.config.ts
+++ xpic-web/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  clearScreen: false,
+  server: {
+    port: 1420,
+    strictPort: true,
+  },
 })
