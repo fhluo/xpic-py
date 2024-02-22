eaa56b936901d9c615253437112148eecf08859b
fhluo <20843637+fhluo@users.noreply.github.com>
2024-02-23 01:28:41 +0800

    feat: use overlayscrollbars

--- xpic-web/src/app.tsx
+++ xpic-web/src/app.tsx
@@ -3,8 +3,10 @@ import "lightgallery/css/lightgallery-bundle.css";
 import lgThumbnail from "lightgallery/plugins/thumbnail";
 import lgZoom from "lightgallery/plugins/zoom";
 import LightGallery from "lightgallery/react";
+import "overlayscrollbars/overlayscrollbars.css";
 import {useEffect, useState} from "react";
 import "./app.css";
+import {OverlayScrollbarsComponent} from "overlayscrollbars-react";
 
 function App() {
     const [wallpapers, setWallpapers] = useState<string[]>([]);
@@ -22,23 +24,27 @@ function App() {
 
     return (
         <>
-            <LightGallery
-                plugins={[lgZoom, lgThumbnail]}
-                speed={500}
-                thumbnail={true}
-            >
-                {wallpapers.map((v) => (
-                    <a href={convertFileSrc(v)} className={"p-3"}>
-                        <img
-                            src={convertFileSrc(v)}
-                            alt={getName(v)}
-                            width={240}
-                            height={135}
-                            className={"rounded hover:brightness-125 hover:drop-shadow"}
-                        />
-                    </a>
-                ))}
-            </LightGallery>
+            <OverlayScrollbarsComponent defer>
+                <div className={"w-screen h-screen"}>
+                    <LightGallery
+                        plugins={[lgZoom, lgThumbnail]}
+                        speed={500}
+                        thumbnail={true}
+                    >
+                        {wallpapers.map((v) => (
+                            <a href={convertFileSrc(v)} className={"p-3"}>
+                                <img
+                                    src={convertFileSrc(v)}
+                                    alt={getName(v)}
+                                    width={240}
+                                    height={135}
+                                    className={"rounded hover:brightness-125 hover:drop-shadow"}
+                                />
+                            </a>
+                        ))}
+                    </LightGallery>
+                </div>
+            </OverlayScrollbarsComponent>
         </>
     );
 }
