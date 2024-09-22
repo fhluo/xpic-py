38f5294c838a0898c056fde64724cca10b9026a7
fhluo <20843637+fhluo@users.noreply.github.com>
2024-09-23 00:36:04 +0800

    chore: bump tailwindcss version to 4.0

--- xpic-web/vite.config.ts
+++ xpic-web/vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [tailwindcss(), svelte()],
   resolve: {
     alias: {
       "$lib": path.resolve("./src/lib"),
